a437ff4d73dda086386459a42e8a3d03d95b4231
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-05-06 11:03:26 +0200

    luffarschack

--- Luffarschack/Program.cs
+++ Luffarschack/Program.cs
@@ -6,10 +6,46 @@ namespace Luffarschack
     {
         static void Main(string[] args)
         {
+            char spelare = 'X';
             char[,] spelplan = new char[3, 3];
-            
-            // initialisering av bordet
+            initialisera(spelplan);
 
+            spelplan[0, 0] = spelare;
+            spelare = 'O';
+            spelplan[2, 2] = spelare;
+            spelare = 'X';
+
+            while (true)
+            {
+                Console.Clear();
+                skrivut(spelplan);
+
+                Console.Write("Snälla ange en rad: ");
+                int rad = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Snälla ange en kolumn: ");
+                int kolumn = Convert.ToInt32(Console.ReadLine());
+
+                spelplan[rad, kolumn] = spelare;
+
+                if (spelare == 'X')
+                {
+                    spelare = 'O';
+                }
+                else
+                {
+                    spelare = 'X';
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// initialisering av spelplanet
+        /// </summary>
+        /// <param name="spelplan"></param>
+        static void initialisera(char[,] spelplan)
+        {
             for (int rad = 0; rad < 3; rad++)
             {
                 for (int kolumn = 0; kolumn < 3; kolumn++)
@@ -17,13 +53,18 @@ namespace Luffarschack
                     spelplan[rad, kolumn] = ' ';
                 }
             }
-            spelplan[0, 0] = 'X';
-            spelplan[2, 2] = 'O'; 
-            
-            // Skriver ut bordet
+        }
+
+        /// <summary>
+        /// Skriver ut spelplanet
+        /// </summary>
+        /// <param name="spelplan"></param>
+        static void skrivut(char[,] spelplan)
+        {
+            Console.WriteLine("  | 0 | 1 | 2 |  ");
             for (int rad = 0; rad < 3; rad++)
             {
-                Console.Write("| ");
+                Console.Write(rad + " | ");
                 for (int kolumn = 0; kolumn < 3; kolumn++)
                 {
                     Console.Write(spelplan[rad, kolumn]);
@@ -32,7 +73,6 @@ namespace Luffarschack
                 Console.WriteLine();
             }
         }
-            
 
     }
 }
